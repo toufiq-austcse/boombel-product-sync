bf0356119035628b6023fcb61fd979e5ff5cc7e9
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2021-06-13 13:35:17 +0600

    done

--- index.js
+++ index.js
@@ -1,143 +1,286 @@
-const { Worker } = require("worker_threads");
+const { Worker } = require('worker_threads');
+let segments = [
+  {
+    code: 0,
+    name: 'Others',
+  },
+  {
+    code: 1,
+    name: 'Bikes',
+  },
+  {
+    code: 2,
+    name: 'Bike parts',
+  },
+  {
+    code: 3,
+    name: 'Toys',
+  },
+  {
+    code: 4,
+    name: 'Sportclubs',
+  },
+  {
+    code: 5,
+    name: 'Sport and Casuals',
+  },
+  {
+    code: 6,
+    name: 'Automotive',
+  },
+  {
+    code: 7,
+    name: 'Eyewear',
+  },
+  {
+    code: 8,
+    name: 'Bookshop',
+  },
+  {
+    code: 9,
+    name: 'Babyshop',
+  },
+  {
+    code: 10,
+    name: 'Outdoorshop',
+  },
+  {
+    code: 11,
+    name: 'Home and Garden',
+  },
+  {
+    code: 12,
+    name: 'Partyshop',
+  },
+  {
+    code: 13,
+    name: 'Petshop',
+  },
+  {
+    code: 14,
+    name: 'Health and Beauty',
+  },
+];
 
 function getProductName(name) {
-  if (!name) return "";
+  if (!name) return '';
   let modifiedName = name.toLowerCase();
   modifiedName[0] = modifiedName[0].toUpperCase();
   return modifiedName;
 }
 function getWeight(shippingSize) {
   switch (shippingSize) {
-    case "XS":
+    case 'XS':
       return 1;
-    case "S":
+    case 'S':
       return 3;
-    case "M":
+    case 'M':
       return 5;
-    case "L":
+    case 'L':
       return 10;
-    case "XL":
+    case 'XL':
       return 20;
     default:
       return 0;
   }
 }
 
-function processProducts(products) {
-  for (let product in products) {
-    let id = Date.now;
-    let modifiedProduct = {
-      ID: id,
-      Type: "Simple",
-      SKU: id,
-      Name: getProductName(product.name_en),
-      Published: 1,
-      is_featured: 0,
-      "Visibility in catalog": "visible",
-      "Short description": "",
-      Description: product.description_en ? processProducts.description_en : "",
-      "Date sale price starts": "",
-      "Date sale price ends": "",
-      "Tax status": "taxable",
-      "Tax class": "",
-      "In stock?": 1,
-      Stock: product.stock ? product.stock : 0,
-      "Low stock amount": "",
-      "Backorders allowed": "",
-      "Sold individually?": 0,
-      "Weight (kg)": getWeight(processProducts.shipping_size),
-      "Length (cm)": "",
-      "Width (cm)": "",
-      "Height (cm)": "",
-      "Allow customer reviews": 0,
-      "Purchase note": "",
-      "Sale price": "",
+function splitProductBySegment(product) {
+  let splittedProducts = [];
+  let { categories } = product;
+  categories.forEach((category) => {
+    let { categories, ...rest } = product;
+    splittedProducts.push({ category, ...rest });
+  });
 
-      //sd
-      "Regular price": "",
-      Categories: "",
-      Tags: "",
-      "Shipping class": "",
-      Images: "",
-      "Download limit": "",
-      "Download expiry days": "",
-      Parent: "",
-      "Grouped products": "",
-      Upsells: "",
-      "Cross-sells": "",
-      "External URL": "",
-      "Button text": "",
-      Position: "",
-      "Meta: _wxr_import_user_slug": "",
-      "Meta: big_store_sidebar_dyn": "",
-      "Attribute 1 name": "",
-      "Attribute 1 value(s)": "",
-      "Attribute 1 visible": "",
-      "Attribute 1 global": "",
-      "Attribute 2 name": "",
-      "Attribute 2 value(s)": "",
-      "Attribute 2 visible": "",
-      "Attribute 2 global": "",
-      "Attribute 3 name": "",
-      "Attribute 3 value(s)": "",
-      "Attribute 3 visible": "",
-      "Attribute 3 global": "",
-      "Attribute 4 name": "",
-      "Attribute 4 value(s)": "",
-      "Attribute 4 visible": "",
-      "Attribute 4 global": "",
-      "Attribute 5 name": "",
-      "Attribute 5 value(s)": "",
-      "Attribute 5 visible": "",
-      "Attribute 5 global": "",
-      "Meta: _last_editor_used_jetpack": "",
-      "Meta: _wp_page_template": "",
-      "Attribute 1 default": "",
-    };
+  return splittedProducts;
+}
+
+function getPrice(product) {
+  let index = segments.findIndex((segment) => segment.code === product.category.parent_segment);
+  if (index < 0) return 0;
+  let totalPrice = 0;
+  let category = segments[index];
+  let { consumer_price } = product;
+  let vat = (21 / 100) * consumer_price;
+  if (category.name === 'Babyshop' || category.name === 'Outdoorshop' || category.name === 'Health and Beauty') {
+    totalPrice = consumer_price + (40 / 100) * consumer_price + vat + 4;
+    return totalPrice;
   }
+
+  totalPrice = consumer_price + (30 / 100) * consumer_price + vat + 4;
+  return totalPrice;
+}
+function getCategory(product) {
+  let { category } = product;
+  let index = segments.findIndex((segment) => segment.code === product.category.parent_segment);
+  if (index < 0) return '';
+  let segment = segments[index];
+  return `${segment.name}>${category.parent_name_en}>${category.name_en}`;
+}
+function getTags(product) {
+  let { brand, category } = product;
+  let tags = `${brand}`;
+  tags += `, ${category.parent_name_en}, ${category.name_en}`;
+  return tags;
+}
+
+function getImages(product) {
+  let { media } = product;
+  media = media.filter((m) => m.type === 'image');
+  let urls = ``;
+  media.forEach((m) => (urls += `${m.url}, `));
+  return urls.trim().slice(0, -1);
+}
+function getColorAttr(product) {
+  let index = product.attributes.findIndex((att) => att.name_en === 'Colour');
+  if (index === -1) return '';
+  return product.attributes[index].value_en;
+}
+
+function getGenderAttr(product) {
+  let index = product.attributes.findIndex((att) => att.name_en === 'Gender');
+  if (index === -1) return '';
+  return product.attributes[index].value_en;
+}
+
+function getMaterialAttr(product) {
+  let index = product.attributes.findIndex((att) => att.name_en === 'Material');
+  if (index === -1) return '';
+  return product.attributes[index].value_en;
+}
+
+function getSizeAttr(product) {
+  let index = product.attributes.findIndex((att) => att.name_en === 'Size');
+  if (index === -1) return '';
+  return product.attributes[index].value_en;
+}
+function processProducts(products) {
+  products.forEach((mainProduct) => {
+    let splittedProducts = splitProductBySegment(mainProduct);
+
+    splittedProducts.forEach((product) => {
+      let id = Date.now;
+      let modifiedProduct = {
+        ID: id,
+        Type: 'Simple',
+        SKU: id,
+        Name: getProductName(product.name_en),
+        Published: 1,
+        is_featured: 0,
+        'Visibility in catalog': 'visible',
+        'Short description': '',
+        Description: product.description_en ? product.description_en : '',
+        'Date sale price starts': '',
+        'Date sale price ends': '',
+        'Tax status': 'taxable',
+        'Tax class': '',
+        'In stock?': 1,
+        Stock: product.stock ? product.stock : 0,
+        'Low stock amount': '',
+        'Backorders allowed': '',
+        'Sold individually?': 0,
+        'Weight (kg)': getWeight(product.shipping_size),
+        'Length (cm)': '',
+        'Width (cm)': '',
+        'Height (cm)': '',
+        'Allow customer reviews': 0,
+        'Purchase note': '',
+        'Sale price': '',
+
+        //sd
+        'Regular price': getPrice(product),
+        Categories: getCategory(product),
+        Tags: getTags(product),
+        'Shipping class': '',
+        Images: getImages(product),
+        'Download limit': '',
+        'Download expiry days': '',
+        Parent: '',
+        'Grouped products': '',
+        Upsells: '',
+        'Cross-sells': '',
+        'External URL': '',
+        'Button text': '',
+        Position: '',
+        'Meta: _wxr_import_user_slug': '',
+        'Meta: big_store_sidebar_dyn': '',
+
+        'Attribute 1 name': 'Brand',
+        'Attribute 1 value(s)': product.brand,
+        'Attribute 1 visible': 1,
+        'Attribute 1 global': 1,
+
+        'Attribute 2 name': 'Color',
+        'Attribute 2 value(s)': getColorAttr(product),
+        'Attribute 2 visible': 1,
+        'Attribute 2 global': 1,
+
+        'Attribute 3 name': 'Gender',
+        'Attribute 3 value(s)': getGenderAttr(product),
+        'Attribute 3 visible': 1,
+        'Attribute 3 global': 1,
+
+        'Attribute 4 name': 'Material',
+        'Attribute 4 value(s)': getMaterialAttr(product),
+        'Attribute 4 visible': 1,
+        'Attribute 4 global': 1,
+
+        'Attribute 5 name': 'Size',
+        'Attribute 5 value(s)': getSizeAttr(product),
+        'Attribute 5 visible': 1,
+        'Attribute 5 global': 1,
+        'Meta: _last_editor_used_jetpack': '',
+        'Meta: _wp_page_template': 'default',
+        'Attribute 1 default': '',
+      };
+      console.log(modifiedProduct);
+    });
+  });
 }
 (async () => {
   let total = 0;
-  let worker1 = new Worker("./worker.js", { workerData: { start: 1, end: 100 } });
-  worker1.on("error", (error) => {
+  let worker1 = new Worker('./worker.js', { workerData: { start: 1, end: 100 } });
+  worker1.on('error', (error) => {
     console.log(error);
   });
 
-  worker1.on("exit", (exitCode) => {
+  worker1.on('exit', (exitCode) => {
     console.log(exitCode);
   });
 
   //Listen for a message from worker
-  worker1.on("message", (result) => {
+  worker1.on('message', (result) => {
     total += 1;
-    console.log("result ", result);
+    console.log('yo');
+    processProducts(result);
   });
 
-  let worker2 = new Worker("./worker.js", { workerData: { start: 101, end: 200 } });
-  worker2.on("error", (error) => {
+  let worker2 = new Worker('./worker.js', { workerData: { start: 101, end: 200 } });
+  worker2.on('error', (error) => {
     console.log(error);
   });
 
-  worker2.on("exit", (exitCode) => {
+  worker2.on('exit', (exitCode) => {
     console.log(exitCode);
   });
 
-  worker2.on("message", (result) => {
+  worker2.on('message', (result) => {
     total += 1;
-    console.log("result ", result);
+    // console.log('result ', result);
   });
 
-  let worker3 = new Worker("./worker.js", { workerData: { start: 201, end: 300 } });
-  worker3.on("error", (error) => {
+  let worker3 = new Worker('./worker.js', { workerData: { start: 201, end: 300 } });
+  worker3.on('error', (error) => {
     console.log(error);
   });
 
-  worker3.on("exit", (exitCode) => {
+  worker3.on('exit', (exitCode) => {
     console.log(exitCode);
   });
 
-  worker3.on("message", (result) => {
+  worker3.on('message', (result) => {
     total += 1;
-    console.log("result ", result);
+    // console.log('result ', result);
   });
 })();
--- worker.js
+++ worker.js
@@ -5,7 +5,7 @@ async function getProducts(page) {
   try {
     let res = await axios.default.get(`https://portal.internet-bikes.com/api/twm/products?page=${page}`, {
       headers: {
-        Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvcG9ydGFsLmludGVybmV0LWJpa2VzLmNvbVwvYXBpXC90d21cL2F1dGhcL2F1dGhlbnRpY2F0ZSIsImlhdCI6MTYyMzQ5NTUxNCwiZXhwIjoxNjIzNDk3MzE0LCJuYmYiOjE2MjM0OTU1MTQsImp0aSI6IlBlRURkdks1eUpmWmVWaWoiLCJzdWIiOjYyNzYxNCwicHJ2IjoiOGI0MjJlNmY2NTc5MzJiOGFlYmNiMWJmMWUzNTZkZDc2YTM2NWJmMiJ9.yOKipT7kiRSn5ScX8AO7VnZHOqql-S3QEWjTVBheaSk`,
+        Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvcG9ydGFsLmludGVybmV0LWJpa2VzLmNvbVwvYXBpXC90d21cL2F1dGhcL2F1dGhlbnRpY2F0ZSIsImlhdCI6MTYyMzU2NzQ4NywiZXhwIjoxNjIzNTY5Mjg3LCJuYmYiOjE2MjM1Njc0ODcsImp0aSI6IjhqeFhvSHdkS01EVWF0ckciLCJzdWIiOjYyNzYxNCwicHJ2IjoiOGI0MjJlNmY2NTc5MzJiOGFlYmNiMWJmMWUzNTZkZDc2YTM2NWJmMiJ9.WJlijZ35yVjSALxdHlI-RmXhBSV3UG_aKzKC_NH0A8o`,
       },
     });
     return res.data;
