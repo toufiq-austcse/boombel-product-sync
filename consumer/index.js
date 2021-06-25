9a17c6573a561de83bb67273495da3127ce7d054
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2021-06-25 18:53:48 +0600

    Changes added

--- consumer/index.js
+++ consumer/index.js
@@ -44,29 +44,28 @@ try {
       console.log('total ', total);
     }
   });
-   let worker3 = new Worker('./worker.js');
+  let worker3 = new Worker('./worker.js');
   worker3.on('error', (error) => {
     console.log(error);
-  }); */
+  });
 
-   worker3.on('exit', (exitCode) => {
-   console.log(exitCode);
-});
+  worker3.on('exit', (exitCode) => {
+    console.log(exitCode);
+  });
 
   //Listen for a message from worker
- worker3.on('message', (result) => {
-    
-  let total = fs.readFileSync(logFilePath, { encoding: 'utf-8' });
-   if (!total) {
-    fs.writeFileSync(logFilePath, '0', { encoding: 'utf-8' });
-  } else {
-     total = parseInt(total) + result;
-     fs.writeFileSync(logFilePath, total.toString(), { encoding: 'utf-8' });
-      #console.log('total ', total);
-  }
-});
+  worker3.on('message', (result) => {
+    let total = fs.readFileSync(logFilePath, { encoding: 'utf-8' });
+    if (!total) {
+      fs.writeFileSync(logFilePath, '0', { encoding: 'utf-8' });
+    } else {
+      total = parseInt(total) + result;
+      fs.writeFileSync(logFilePath, total.toString(), { encoding: 'utf-8' });
+      console.log('total ', total);
+    }
+  });
 
-/*let worker4 = new Worker('./worker.js');
+  /*let worker4 = new Worker('./worker.js');
 worker4.on('error', (error) => {
    console.log(error);
 });
